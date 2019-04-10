bbd8a3a03d8c230284c5050d40d2c53a46d4682e
Tazio <Tazi0@bitbucket.org>
2019-04-10 18:38:32 +0200

    reverting back

--- new_banking/server.lua
+++ new_banking/server.lua
@@ -8,7 +8,7 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 RegisterServerEvent('bank:deposit')
 AddEventHandler('bank:deposit', function(amount)
 	local _source = source
-	
+
 	local xPlayer = ESX.GetPlayerFromId(_source)
 	if amount == nil or amount <= 0 or amount > xPlayer.getMoney() then
 		TriggerClientEvent('chatMessage', _source, "Invalid Amount")
@@ -40,7 +40,7 @@ AddEventHandler('bank:balance', function()
 	local xPlayer = ESX.GetPlayerFromId(_source)
 	balance = xPlayer.getAccount('bank').money
 	TriggerClientEvent('currentbalance1', _source, balance)
-	
+
 end)
 
 
@@ -48,25 +48,52 @@ RegisterServerEvent('bank:transfer')
 AddEventHandler('bank:transfer', function(to, amountt)
 	local _source = source
 	local xPlayer = ESX.GetPlayerFromId(_source)
-	local zPlayer = ESX.GetPlayerFromId(to)
+	local zPlayer = ESX.GetPlayerFromId(tonumber(to))
 	local balance = 0
 	balance = xPlayer.getAccount('bank').money
 	zbalance = zPlayer.getAccount('bank').money
-	
+
 	if tonumber(_source) == tonumber(to) then
 		TriggerClientEvent('chatMessage', _source, "You cannot transfer to your self")
 	else
-		if balance <= 0 or balance < tonumber(amountt) or tonumber(amountt) <= 0 then
+		if balance < 1 or balance < tonumber(amountt) or tonumber(amountt) <= 0 then
 			TriggerClientEvent('chatMessage', _source, "You don't have enough money in the bank.")
 		else
-			xPlayer.removeAccountMoney('bank', amountt)
-			zPlayer.addAccountMoney('bank', amountt)
+			xPlayer.removeAccountMoney('bank', tonumber(amountt))
+			zPlayer.addAccountMoney('bank', tonumber(amountt))
 		end
-		
+
 	end
 end)
 
+-- version checker
+local CurrentVersion = '1.1'
+local GithubResourceName = 'new_banking'
+local githubacct = "Tazi0"
+local resourceName = GetCurrentResourceName()
+local versionurl = "https://raw.githubusercontent.com/"..githubacct.."/"..GithubResourceName.."/master/VERSION"
+local changesurl = "https://raw.githubusercontent.com/"..githubacct.."/"..GithubResourceName.."/master/CHANGES"
 
-
-
-
+PerformHttpRequest(versionurl, function(Error, NewestVersion, Header)
+	PerformHttpRequest(changesurl, function(Error, Changes, Header)
+		print('\n')
+		print('====================================================================')
+		print('')
+		print('New Banking ('..resourceName..')')
+		print('')
+		print('Current Version: ' .. CurrentVersion)
+		print('Newest Version: ' .. NewestVersion)
+		print('you can download the newest version at: \n https://github.com/'..githubacct.."/"..GithubResourceName.."/")
+		io.write("")
+		print('Changelog: \n' .. Changes)
+		print('')
+		if CurrentVersion ~= NewestVersion then
+			print('====================================================================')
+		else
+			print('===================')
+			print('=== Up to date! ===')
+			print('===================')
+		end
+		print('\n')
+end)
+end)
