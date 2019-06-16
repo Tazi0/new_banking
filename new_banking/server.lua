580ec1a21affdf439612d25b00a38537487f7219
abdulkadiraktas <abdulkadir.aktas@hotmail.com>
2019-06-16 21:34:27 +0300

    Fix Error
    Fix this error.
    Error running call reference function for resource new_banking: citizen:/scripting/lua/scheduler.lua:405: @new_banking/server.lua:54: attempt to index a nil value (local 'user')
    Error running system event handling function for resource essentialmode: citizen:/scripting/lua/scheduler.lua:41: Failed to execute thread: citizen:/scripting/lua/MessagePack.lua:830: missing bytes

--- new_banking/server.lua
+++ new_banking/server.lua
@@ -1,77 +1,80 @@
---================================================================================================
---==                                VARIABLES - DO NOT EDIT                                     ==
---================================================================================================
+-- ================================================================================================
+-- ==                                VARIABLES - DO NOT EDIT                                     ==
+-- ================================================================================================
 ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 RegisterServerEvent('bank:deposit')
 AddEventHandler('bank:deposit', function(amount)
-	local _source = source
-	
-	local xPlayer = ESX.GetPlayerFromId(_source)
-	if amount == nil or amount <= 0 or amount > xPlayer.getMoney() then
-		TriggerClientEvent('chatMessage', _source, "Invalid Amount")
-	else
-		xPlayer.removeMoney(amount)
-		xPlayer.addAccountMoney('bank', tonumber(amount))
-	end
-end)
+    local _source = source
 
+    local xPlayer = ESX.GetPlayerFromId(_source)
+    if amount == nil or amount <= 0 or amount > xPlayer.getMoney() then
+        TriggerClientEvent('chatMessage', _source, "Invalid Amount")
+    else
+        xPlayer.removeMoney(amount)
+        xPlayer.addAccountMoney('bank', tonumber(amount))
+    end
+end)
 
 RegisterServerEvent('bank:withdraw')
 AddEventHandler('bank:withdraw', function(amount)
-	local _source = source
-	local xPlayer = ESX.GetPlayerFromId(_source)
-	local base = 0
-	amount = tonumber(amount)
-	base = xPlayer.getAccount('bank').money
-	if amount == nil or amount <= 0 or amount > base then
-		TriggerClientEvent('chatMessage', _source, "Invalid Amount")
-	else
-		xPlayer.removeAccountMoney('bank', amount)
-		xPlayer.addMoney(amount)
-	end
+    local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+    local base = 0
+    amount = tonumber(amount)
+    base = xPlayer.getAccount('bank').money
+    if amount == nil or amount <= 0 or amount > base then
+        TriggerClientEvent('chatMessage', _source, "Invalid Amount")
+    else
+        xPlayer.removeAccountMoney('bank', amount)
+        xPlayer.addMoney(amount)
+    end
 end)
 
 RegisterServerEvent('bank:balance')
 AddEventHandler('bank:balance', function()
-	local _source = source
-	local xPlayer = ESX.GetPlayerFromId(_source)
-	balance = xPlayer.getAccount('bank').money
-	TriggerClientEvent('currentbalance1', _source, balance)
-	
-end)
+    local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+    balance = xPlayer.getAccount('bank').money
+    TriggerClientEvent('currentbalance1', _source, balance)
 
+end)
 
 RegisterServerEvent('bank:transfer')
 AddEventHandler('bank:transfer', function(to, amountt)
-	local _source = source
-	local xPlayer = ESX.GetPlayerFromId(_source)
-	local zPlayer = ESX.GetPlayerFromId(to)
-	
-	--Thanks to (LuCampbell)
-	TriggerEvent('es:getPlayerFromId', xPlayer, function(user)
-		if (tonumber(user.money) >= tonumber(amountt)) then
-			local player = user.identifier
-			user:removeMoney((amountt))	
-		
-			TriggerEvent('es:getPlayerFromId', zPlayer, function(user2)
-				local player2 = user2.identifier
-				user2:addMoney((amountt))
-				TriggerClientEvent("chatMessage", zPlayer , "You received money ", { 52, 201, 36 }, "You received the sum of "..amountt.." dollars")
-				TriggerClientEvent("chatMessage", xPlayer, "Payment receipt ", { 255, 0, 0 }, "Your payment of "..amountt.." dollars is done")
-			end)	
-		else
-			if (tonumber(user.money) < tonumber(amountt)) then
-			
-				TriggerClientEvent("chatMessage", player, "", { 255, 0, 0 }, "You do not have enough money")
-			end
-		end
-	end) 
+    local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+    local zPlayer = ESX.GetPlayerFromId(to)
+
+    -- Thanks to (LuCampbell)
+	TriggerEvent('es:getPlayerFromId', xPlayer, function(user) 
+        if zPlayer ~= nil then
+            if (tonumber(user.money) >= tonumber(amountt)) then
+                local player = user.identifier
+                user:removeMoney((amountt))
+
+                TriggerEvent('es:getPlayerFromId', zPlayer, function(user2)
+                    local player2 = user2.identifier
+                    user2:addMoney((amountt))
+                    TriggerClientEvent("chatMessage", zPlayer,
+                                       "You received money ", {52, 201, 36},
+                                       "You received the sum of " .. amountt ..
+                                           " dollars")
+                    TriggerClientEvent("chatMessage", xPlayer,
+                                       "Payment receipt ", {255, 0, 0},
+                                       "Your payment of " .. amountt ..
+                                           " dollars is done")
+                end)
+            else
+                if (tonumber(user.money) < tonumber(amountt)) then
+
+                    TriggerClientEvent("chatMessage", player, "", {255, 0, 0},
+                                       "You do not have enough money")
+                end
+            end
+        end
+    end)
 end)
-
-
-
-
 
