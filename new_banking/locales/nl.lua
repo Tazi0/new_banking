9ccc6371223eb03e2076d7eb447baa01297efa34
Tazio <tazio2003@gmail.com>
2019-06-16 21:52:44 +0200

    Added main files

---
+++ new_banking/locales/nl.lua
@@ -0,0 +1,13 @@
+Locales['nl'] = {
+    ['invalid_amount'] = 'Ongeldig Hoeveelheid',
+    ['atm_blip'] = 'Geldautomaat',
+    ['bank_blip'] = 'Bank',
+    ['atm_open'] = 'Druk ~INPUT_PICKUP~ om uw account te openen ~b~',
+    ['no_money'] = 'U heeft niet genoeg geld',
+    ['recieved1'] = 'Je hebt geld ontvangen ',
+    ['recieved2'] = 'Je hebt een hoeveelheid ontvangen van ',
+    ['recieved3'] = ' euro',
+    ['removed1'] = 'Betalingsbewijs ',
+    ['removed2'] = 'Uw betaling van ',
+    ['removed3'] = ' euro is doorgegeven'
+}
