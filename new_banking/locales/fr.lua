620db679ac8453c5aed4acc3965e14f5ed4fb8ed
Snipeur060 <61784407+Snipeur060@users.noreply.github.com>
2022-08-18 17:17:55 +0200

    Create fr.lua
    Add french local :-)

---
+++ new_banking/locales/fr.lua
@@ -0,0 +1,13 @@
+Locales['ft'] = {
+    ['invalid_amount'] = 'Montant invalide',
+    ['atm_blip'] = 'DAB',
+    ['bank_blip'] = 'Banque',
+    ['atm_open'] = 'Press ~INPUT_PICKUP~ pour accéder au compte ~b~',
+    ['no_money'] = 'Vous n\'avez pas assez d\'argent',
+    ['transfer_money'] = 'Transférer de l\'argent',
+    ['bank'] = 'Banque',
+    ['transfer_to'] = 'Vous avez transféré ~r~$ %s~s~ à ~r~%s.',
+    ['receive_money'] = 'Vous avez reçu ~r~$ %s~s~ de la part de ~r~%s.',
+    ['not_to_yourself'] = 'Vous ne pouvez pas transférer de l\'argent à vous même!',
+    ['not_enough_money'] = 'Pas assez d\'argent pour effectuer le transfert !'
+}
